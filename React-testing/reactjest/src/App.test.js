e681136c3f5ab717a5296f466442dfb7d4f0dbcf
Raja Rajeswari Tangirala <rajeswari.tangirala@cloud4c.com>
2022-11-29 20:10:40 +0530

    added test

--- React-testing/reactjest/src/App.test.js
+++ React-testing/reactjest/src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders and save to reload', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/and save to reload/i);
+  expect(linkElement).toBeInTheDocument();
+});
